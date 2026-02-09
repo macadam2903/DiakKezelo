d2317659197b3661ff53a6d88f8be8c5ae0c3a71
macadam2903 <macadammaci@gmail.com>
2026-02-09 13:05:32 +0100

    Add project files.

---
+++ DiakKezelo/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace DiakKezelo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCsvTitle = new System.Windows.Forms.Label();
+            this.btnSelectCsv = new System.Windows.Forms.Button();
+            this.btnLoadCsv = new System.Windows.Forms.Button();
+            this.dgvStudents = new System.Windows.Forms.DataGridView();
+            this.lblRegTitle = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCsvTitle
+            // 
+            this.lblCsvTitle.AutoSize = true;
+            this.lblCsvTitle.Location = new System.Drawing.Point(117, 35);
+            this.lblCsvTitle.Name = "lblCsvTitle";
+            this.lblCsvTitle.Size = new System.Drawing.Size(94, 13);
+            this.lblCsvTitle.TabIndex = 0;
+            this.lblCsvTitle.Text = "CSV Fájl Betöltése";
+            // 
+            // btnSelectCsv
+            // 
+            this.btnSelectCsv.Location = new System.Drawing.Point(98, 60);
+            this.btnSelectCsv.Name = "btnSelectCsv";
+            this.btnSelectCsv.Size = new System.Drawing.Size(125, 23);
+            this.btnSelectCsv.TabIndex = 1;
+            this.btnSelectCsv.Text = "CSV Kiválasztása";
+            this.btnSelectCsv.UseVisualStyleBackColor = true;
+            this.btnSelectCsv.Click += new System.EventHandler(this.btnSelectCsv_Click);
+            // 
+            // btnLoadCsv
+            // 
+            this.btnLoadCsv.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLoadCsv.Location = new System.Drawing.Point(270, 60);
+            this.btnLoadCsv.Name = "btnLoadCsv";
+            this.btnLoadCsv.Size = new System.Drawing.Size(144, 23);
+            this.btnLoadCsv.TabIndex = 2;
+            this.btnLoadCsv.Text = "Betöltés Adatbázisba";
+            this.btnLoadCsv.UseVisualStyleBackColor = true;
+            this.btnLoadCsv.Click += new System.EventHandler(this.btnLoadCsv_Click);
+            // 
+            // dgvStudents
+            // 
+            this.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudents.Location = new System.Drawing.Point(98, 141);
+            this.dgvStudents.Name = "dgvStudents";
+            this.dgvStudents.Size = new System.Drawing.Size(600, 250);
+            this.dgvStudents.TabIndex = 3;
+            // 
+            // lblRegTitle
+            // 
+            this.lblRegTitle.AutoSize = true;
+            this.lblRegTitle.Location = new System.Drawing.Point(113, 418);
+            this.lblRegTitle.Name = "lblRegTitle";
+            this.lblRegTitle.Size = new System.Drawing.Size(111, 13);
+            this.lblRegTitle.TabIndex = 4;
+            this.lblRegTitle.Text = "Új Diák Regisztrációja";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(113, 460);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(30, 13);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Név:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(154, 457);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(192, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(113, 487);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 7;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(154, 486);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(192, 20);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(113, 519);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(26, 13);
+            this.lblAge.TabIndex = 8;
+            this.lblAge.Text = "Kor:";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(154, 515);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(43, 20);
+            this.txtAge.TabIndex = 6;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(146, 564);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 9;
+            this.btnSubmit.Text = "Regisztráció";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(339, 564);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 10;
+            this.btnRefresh.Text = "Frissítés";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 641);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblRegTitle);
+            this.Controls.Add(this.dgvStudents);
+            this.Controls.Add(this.btnLoadCsv);
+            this.Controls.Add(this.btnSelectCsv);
+            this.Controls.Add(this.lblCsvTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCsvTitle;
+        private System.Windows.Forms.Button btnSelectCsv;
+        private System.Windows.Forms.Button btnLoadCsv;
+        private System.Windows.Forms.DataGridView dgvStudents;
+        private System.Windows.Forms.Label lblRegTitle;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ DiakKezelo/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using CsvHelper;
+using System.IO;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace DiakKezelo
+{
+    public partial class Form1 : Form
+    {
+        // adatb
+        string connectionString = "Server=localhost;Port=3307;Database=diak_adatbazis;Uid=root;Pwd=;";
+        string selectedCsvPath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadStudentsToGrid();
+        }
+
+        // openfiledialoh
+        private void btnSelectCsv_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV fájlok (*.csv)|*.csv";
+            openFileDialog.Title = "Válassz CSV fájlt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedCsvPath = openFileDialog.FileName;
+                MessageBox.Show("Fájl kiválasztva: " + Path.GetFileName(selectedCsvPath));
+            }
+        }
+
+        // readin
+        private void btnLoadCsv_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedCsvPath))
+            {
+                MessageBox.Show("Először válassz ki egy CSV fájlt!");
+                return;
+            }
+
+            try
+            {
+                // csv read
+                List<Student> students = new List<Student>();
+
+                StreamReader reader = new StreamReader(selectedCsvPath);
+                CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+                var records = csv.GetRecords<Student>();
+                foreach (var record in records)
+                {
+                    students.Add(record);
+                }
+
+                reader.Close();
+
+                // adatb save
+                MySqlConnection conn = new MySqlConnection(connectionString);
+                conn.Open();
+
+                int count = 0;
+                foreach (Student student in students)
+                {
+                    string query = "INSERT INTO Students (Name, Email, Age) VALUES (@Name, @Email, @Age)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                    cmd.Parameters.AddWithValue("@Name", student.Name);
+                    cmd.Parameters.AddWithValue("@Email", student.Email);
+                    cmd.Parameters.AddWithValue("@Age", student.Age);
+
+                    cmd.ExecuteNonQuery();
+                    count++;
+                }
+
+                conn.Close();
+
+                MessageBox.Show(count + " diák sikeresen importálva!");
+                LoadStudentsToGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba: " + ex.Message);
+            }
+        }
+
+        // reg
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // check
+            if (string.IsNullOrWhiteSpace(txtName.Text) ||
+                string.IsNullOrWhiteSpace(txtEmail.Text) ||
+                string.IsNullOrWhiteSpace(txtAge.Text))
+            {
+                MessageBox.Show("Minden mezőt ki kell tölteni!");
+                return;
+            }
+
+            int age = 0;
+            if (!int.TryParse(txtAge.Text, out age))
+            {
+                MessageBox.Show("Az életkor számnak kell lennie!");
+                return;
+            }
+
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(connectionString);
+                conn.Open();
+
+                string query = "INSERT INTO Students (Name, Email, Age) VALUES (@Name, @Email, @Age)";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@Age", age);
+
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                MessageBox.Show("Diák sikeresen regisztrálva!");
+
+                
+                txtName.Clear();
+                txtEmail.Clear();
+                txtAge.Clear();
+
+                LoadStudentsToGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba: " + ex.Message);
+            }
+        }
+
+        // grid refresh
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadStudentsToGrid();
+        }
+
+
+        private void LoadStudentsToGrid()
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(connectionString);
+                conn.Open();
+
+                string query = "SELECT * FROM Students ORDER BY Id DESC";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                dgvStudents.DataSource = dt;
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba: " + ex.Message);
+            }
+        }
+    }
+}
